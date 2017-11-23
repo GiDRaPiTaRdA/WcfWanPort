ad235ccda97879bb4e3a81f00ed9cf574c5f9d89
GiDRaPiTaRdA <xxmaxdrivezz@gmail.com>
2017-11-24 00:21:44 +0200

    first

---
+++ ConsoleApplicationWCF/Program.cs
@@ -0,0 +1,21 @@
+﻿//using ConsoleApplicationWCF.ServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationWCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Service1Client client = new Service1Client();
+
+            //Console.WriteLine(client.GetData(100));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleHosting/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost selfHost = new ServiceHost(typeof(Service),new Uri("http://10.0.0.184:54508"));
+
+            try
+            {
+              
+                selfHost.Open();
+
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine(selfHost.BaseAddresses.First());
+#if DEBUG
+                Console.ReadLine();
+#endif
+                // Close the ServiceHostBase to shutdown the service.  
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
